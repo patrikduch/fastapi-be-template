890081d0788fe15146e853c86164511851ed3b78
patrikduch <duchpatrik@icloud.com>
2024-06-04 21:28:21 +0200

    Test of Env variables

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
+load_dotenv()  # Load environment variables from .env file
+
+DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
+
 @app.get("/health")
 async def checkHealth():
     return {"message": "healthy"}
+
+
+@app.get("/env")
+async def env_variables():
+    return {"DEBUG": DEBUG}
